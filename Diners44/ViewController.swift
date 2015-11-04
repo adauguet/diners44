4cce0604be5761dbd53a654ee2dd22f0949af585
Antoine DAUGUET <dauguet.antoine@gmail.com>
2015-11-04 17:52:26 +0100

    Initial Commit

---
+++ Diners44/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diners44
+//
+//  Created by Claire DAUGUET on 04/11/2015.
+//  Copyright © 2015 Antoine DAUGUET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
